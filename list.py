ca1b5e61412f5afca4bf066c9bf855caf7402e41
sumeetsuryavanshi8-crypto <sumeetsuryavanshi8@gmail.com>
2026-07-19 09:21:28 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,18 @@
+#list
+students = ["Rahul", "Sumeet", "Anjali", "Rohan", "Priya"]
+print(len(students))
+print(students[0])
+print(students[-1])
+
+students.append("Neha")
+students.remove("Rahul")
+
+print(students)
+
+students.sort()
+
+print(students)
+
+empty_list = []
+print(type(empty_list))
+
---
+++ string.py
@@ -0,0 +1,38 @@
+#strings
+str="suryavanshi"
+lenght=len(str)
+print(str[5])
+print(str[9])
+print(lenght)
+#string concatention 
+print("Hublli" + "Darawada")
+#slicing
+str="andaman nicobar"
+print(str[0:6])
+print(str[6:9])
+print(str[-1:-8])
+
+str=input("Enter a value:")
+mid=len(str)//2
+output2=str[-2]
+print(output2)
+#common string methods
+str="sumeet suryavanshi"
+print(str.upper())
+print(str.lower())
+print(str.title())
+print(str.find("me"))
+print(str.replace("sumeet","rahul"))
+print(str.count("t"))
+#formate string
+name="sumeet"
+age=22
+print(f"my name is {name} and I am {age} years old")
+print("Hello World")
+print("Hello\nWorld")
+print("Hello\tWorld")
+print("Hello" + "Baby")
+print("yum !" * 3)
+print(len("Education"))
+
+
